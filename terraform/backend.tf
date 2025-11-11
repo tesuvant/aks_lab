4eed875f6c9fe56fa860383e3cd872bec99aa459
Teemu Suvanto <teemu.suvanto@gmail.com>
2025-11-11 11:58:13 +0100

    rename backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,11 +2,7 @@ terraform {
   required_version = ">= 1.12.0"
 
   backend "azurerm" {
-    resource_group_name  = "aks"
-    storage_account_name = "827be54aaks"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_azuread_auth     = true
+    use_azuread_auth = true
   }
 }
 
