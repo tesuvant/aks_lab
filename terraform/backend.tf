9787112f667ea8ed871752f1f34f11020c7233eb
Teemu Suvanto <teemu.suvanto@gmail.com>
2025-11-11 13:43:56 +0100

    fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,12 @@ terraform {
   required_version = ">= 1.12.0"
 
   backend "azurerm" {
-    use_azuread_auth = true
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
+    use_azuread_auth     = true
   }
+
 }
 
