1e3a537be522a1b25e2f00fb2099e5f3ae142ece
Teemu Suvanto <teemu.suvanto@gmail.com>
2025-11-19 12:10:49 +0100

    fix plan

--- terraform/finops_function.tf
+++ terraform/finops_function.tf
@@ -3,32 +3,34 @@ data "azurerm_storage_account" "sa" {
   resource_group_name = var.rg_name
 }
 
-data "azurerm_linux_virtual_machine" "vm" {
+data "azurerm_virtual_machine" "vm" {
   name                = "foobar"
   resource_group_name = var.rg_name
 }
 
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   name                = "finops-function-app-plan"
   location            = var.location
   resource_group_name = var.rg_name
   kind                = "FunctionApp"
   sku_name            = "Y1"
+  os_type             = "Windows"
 }
 
-resource "azurerm_function_app" "function_app" {
+resource "azurerm_windows_function_app" "function_app" {
   name                 = "shutdown-function"
   location             = var.location
   resource_group_name  = var.rg_name
-  app_service_plan_id  = azurerm_app_service_plan.plan.id
+  service_plan_id      = azurerm_service_plan.plan.id
   storage_account_name = var.storage_account_name
+  site_config {}
 
-  version       = "~4"
-  os_type       = "Windows"
-  runtime_stack = "powershell"
+  # version       = "~4"
+  # os_type       = "Windows"
+  # runtime_stack = "powershell"
 
   app_settings = {
-    AKS_NAME                 = var.aks_cluster_name
+    AKS_NAME                 = var.aks_name
     AzureWebJobsStorage      = data.azurerm_storage_account.sa.primary_connection_string
     FUNCTIONS_WORKER_RUNTIME = "powershell"
     RESOURCE_GROUP           = var.rg_name
@@ -44,24 +46,10 @@ resource "azurerm_function_app" "function_app" {
   # checkov:skip=CKV_AZURE_67: latest http version
 }
 
-resource "azurerm_role_assignment" "aks_access" {
-  scope                = azurerm_kubernetes_cluster.aks.id
-  role_definition_name = "Contributor"
-  principal_id         = azurerm_function_app.function_app.identity.principal_id
-}
-
-resource "azurerm_role_assignment" "vm_access" {
-  scope                = data.azurerm_linux_virtual_machine.vm.id
-  role_definition_name = "Virtual Machine Contributor"
-  principal_id         = azurerm_function_app.function_app.identity.principal_id
-}
-
-
-resource "azurerm_function_app_slot" "slot" {
+resource "azurerm_windows_function_app_slot" "slot" {
   name            = "production"
-  function_app_id = azurerm_function_app.function.id
-  app_settings    = var.app_settings
-
+  function_app_id = azurerm_windows_function_app.function.id
+  site_config {}
   # checkov:skip=CKV_AZURE_56: auth enabled
   # checkov:skip=CKV_AZURE_70: https only
   # checkov:skip=CKV_AZURE_67: latest http version
@@ -83,4 +71,17 @@ resource "azurerm_function_app_function" "timer_trigger" {
     ]
 }
 CONFIG
+}
+
+
+resource "azurerm_role_assignment" "aks_access" {
+  scope                = azurerm_kubernetes_cluster.aks.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_function_app.function_app.identity.principal_id
+}
+
+resource "azurerm_role_assignment" "vm_access" {
+  scope                = data.azurerm_linux_virtual_machine.vm.id
+  role_definition_name = "Virtual Machine Contributor"
+  principal_id         = azurerm_function_app.function_app.identity.principal_id
 }
