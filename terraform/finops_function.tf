5f30966f933049a8341645ed3c385573c9a703dc
Teemu Suvanto <teemu.suvanto@gmail.com>
2025-11-19 11:32:24 +0100

    fix fmt

--- terraform/finops_function.tf
+++ terraform/finops_function.tf
@@ -17,14 +17,14 @@ resource "azurerm_app_service_plan" "plan" {
 }
 
 resource "azurerm_function_app" "function_app" {
-  name                       = "shutdown-function"
-  location                   = var.location
-  resource_group_name        = var.rg_name
-  app_service_plan_id        = azurerm_app_service_plan.plan.id
-  storage_account_name       = var.storage_account_name
+  name                 = "shutdown-function"
+  location             = var.location
+  resource_group_name  = var.rg_name
+  app_service_plan_id  = azurerm_app_service_plan.plan.id
+  storage_account_name = var.storage_account_name
 
-  version    = "~4"
-  os_type    = "Windows"
+  version       = "~4"
+  os_type       = "Windows"
   runtime_stack = "powershell"
 
   app_settings = {
@@ -42,22 +42,22 @@ resource "azurerm_function_app" "function_app" {
 }
 
 resource "azurerm_role_assignment" "aks_access" {
-  scope              = azurerm_kubernetes_cluster.aks.id
-  role_definition_name = "Contributor"                  
-  principal_id       = azurerm_function_app.function_app.identity.principal_id
+  scope                = azurerm_kubernetes_cluster.aks.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_function_app.function_app.identity.principal_id
 }
 
 resource "azurerm_role_assignment" "vm_access" {
-  scope              = data.azurerm_linux_virtual_machine.vm.id
-  role_definition_name = "Virtual Machine Contributor"    
-  principal_id       = azurerm_function_app.function_app.identity.principal_id
+  scope                = data.azurerm_linux_virtual_machine.vm.id
+  role_definition_name = "Virtual Machine Contributor"
+  principal_id         = azurerm_function_app.function_app.identity.principal_id
 }
 
 
 resource "azurerm_function_app_slot" "slot" {
-  name                  = "production"
-  function_app_id       = azurerm_function_app.function.id
-  app_settings          = var.app_settings
+  name            = "production"
+  function_app_id = azurerm_function_app.function.id
+  app_settings    = var.app_settings
 }
 
 resource "azurerm_function_app_function" "timer_trigger" {
