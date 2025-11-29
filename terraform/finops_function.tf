2e7a2468d2fe9890d4b0ad28a08a72ed8d0208bf
Teemu Suvanto <teemu.suvanto@gmail.com>
2025-11-29 17:33:25 +0100

    script

--- terraform/finops_function.tf
+++ terraform/finops_function.tf
@@ -125,7 +125,7 @@ resource "null_resource" "upload_function" {
   provisioner "local-exec" {
     command = <<CMD
 az functionapp deployment source config-zip \
-  --clean true \
+  --clean \
   --resource-group ${var.rg_name} \
   --name ${azurerm_windows_function_app.function_app.name} \
   --src ${path.module}/function_package.zip \
